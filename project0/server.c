62b0069dff6cf804cda9b7b780968740489bdddf
Hun Namkoong <hnamkoong1@gmail.com>
2015-03-29 16:07:32 +0900

    server is going on

--- project0/server.c
+++ project0/server.c
@@ -52,25 +52,68 @@ typedef struct {
     char buf[RIO_BUFSIZE];
 }rio_t;
 
+// interrupt handler
+void sigchld_handler(int sig);
+
+// open listenfd
+int open_listenfd(int port);
+
+int open_listenfd(int port)
+{
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+          
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        return -1;
+ 
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int)) < 0)
+        return -1;
+
+    /* Listenfd will be an endpoint for all requests to port
+              on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, 1024) < 0)
+        return -1;
+    return listenfd;
+}
+
+
 void main(int argc, char **argv)
 {
-    char *ip;
-    int port, proto;
-    int fd_from_client;
-    int i;
+    int port, i;
+    int listenfd, connfd;
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen; 
 
     for(i=1; i<argc; i++) {
-        if(!strcmp(argv[i], "-h")) {
-            ip = argv[++i];
-        }
     	if(!strcmp(argv[i], "-p")) {
             port = atoi(argv[++i]);
         }
-        if(!strcmp(argv[i], "-m")) {
-            proto = atoi(argv[++i]); 
-        }
     }
 
+    signal(SIGCHLD, sigchld_handler);
+    listenfd = open_listenfd(port);
+    while(1) {
+        clientlen = sizeof(clientaddr);
+        connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
+        if(fork() == 0) {
+            close(listenfd);
+            // use connfd
+            printf("new connection : %d\n", connfd);
+            close(connfd);
+            exit(0);
+        }
+        close(connfd);
+    }
 //    fd_from_client = open_clientfd(ip, port);
 //
 //    // phase 1 - hand shake first
@@ -79,7 +122,14 @@ void main(int argc, char **argv)
 //    // phase 2
 //    phase2(fd_from_client, proto);
 
-    close(fd_from_client);
 }
+
+void sigchld_handler(int sig)
+{
+    while(waitpid(-1, 0, WNOHANG) > 0)
+        ;
+    return;
+}
+
 
 
